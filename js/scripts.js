d8bffcbed7b1f93b3093c6cd21fa10043c8b87e4
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-12-15 15:53:21 -0800

    update transitions to more site content

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   var options = [
-    {selector: '.tagline', offset: 10, callback: function(el) {
+    {selector: '.logo', offset: 10, callback: function(el) {
        Materialize.fadeInImage($(el));
      } },
-    {selector: '.social', offset: 150, callback: function(el) {
+    {selector: '.tagline', offset: 20, callback: function(el) {
        Materialize.fadeInImage($(el));
      } },
-    {selector: '.projects', offset: 400, callback: function(el) {
+    {selector: '.social', offset: 225, callback: function(el) {
+       Materialize.fadeInImage($(el));
+     } },
+    {selector: '.projects', offset: 300, callback: function(el) {
+       Materialize.fadeInImage($(el));
+     } },
+    {selector: '.footer-text', offset: 10, callback: function(el) {
        Materialize.fadeInImage($(el));
      } }
  ];
