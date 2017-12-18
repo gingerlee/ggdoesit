edbe7c6502778f92b7ccca9b2f536926b6326f16
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-12-17 16:54:32 -0800

    update projects, take out scroll span

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
   var options = [
     {selector: '.logo', offset: 10, callback: function(el) {
-       Materialize.fadeInImage($(el));
-     } },
+      Materialize.fadeInImage($(el));
+    } },
     {selector: '.tagline', offset: 20, callback: function(el) {
-       Materialize.fadeInImage($(el));
-     } },
+      Materialize.fadeInImage($(el));
+    } },
     {selector: '.social', offset: 225, callback: function(el) {
-       Materialize.fadeInImage($(el));
-     } },
+      Materialize.fadeInImage($(el));
+    } },
     {selector: '.projects', offset: 300, callback: function(el) {
-       Materialize.fadeInImage($(el));
-     } },
+      Materialize.fadeInImage($(el));
+    } },
     {selector: '.footer-text', offset: 10, callback: function(el) {
-       Materialize.fadeInImage($(el));
-     } }
- ];
+      Materialize.fadeInImage($(el));
+    } }
+  ];
 
  Materialize.scrollFire(options);
 
+//   $('#social').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('.social')).offset().top}, 800, 'linear');
+//   });
+//
 });
